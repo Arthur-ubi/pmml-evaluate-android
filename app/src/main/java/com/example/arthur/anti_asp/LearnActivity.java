9a038f1c403bdd82b45f8c6e1d8baf23d773f818
arthur-ubi <arthur@ubi.cs.ritsumei.ac.jp>
2018-09-16 12:26:48 +0900

    add sampling button

---
+++ app/src/main/java/com/example/arthur/anti_asp/LearnActivity.java
@@ -0,0 +1,13 @@
+package com.example.arthur.anti_asp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LearnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn);
+    }
+}
