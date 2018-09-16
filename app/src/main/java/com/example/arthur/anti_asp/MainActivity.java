20e5b8d01feb6661ff5d96c5bd3c581d4d782134
arthur-ubi <arthur@ubi.cs.ritsumei.ac.jp>
2018-09-16 12:12:16 +0900

    add sampling button

--- app/src/main/java/com/example/arthur/anti_asp/MainActivity.java
+++ app/src/main/java/com/example/arthur/anti_asp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.arthur.anti_asp;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.arthur.anti_asp.R;
@@ -14,10 +19,10 @@ import com.example.arthur.anti_asp.R;
 import static java.lang.Math.sqrt;
 
 public class MainActivity extends Activity implements SensorEventListener {
-    private SensorManager manager;
+  private SensorManager manager;
   private TextView values;
   /** Called when the activity is first created. */
-          @Override
+  @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
@@ -25,12 +30,55 @@ public class MainActivity extends Activity implements SensorEventListener {
     values = (TextView)findViewById(R.id.text_view);
     manager = (SensorManager)getSystemService(SENSOR_SERVICE);
 
-    String filename = "acc.csv";
+
+    Button buttonSave_s = findViewById(R.id.button_l_stand);
+    buttonSave_s.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        //String text = editText.getText().toString();
+        String text = "This is values of stand";
+        saveFile("acc_stand", text);
+      }
+    });
+    Button buttonSave_w = findViewById(R.id.button_l_walk);
+    buttonSave_w.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String text = "This is values of stand";
+        saveFile("acc_walk", text);
+      }
+    });
+    Button buttonSave_r = findViewById(R.id.button_l_run);
+    buttonSave_r.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String text = "This is values of stand";
+        saveFile("acc_run", text);
+      }
+    });
+
+  }
+
+
+
+  //ファイル保存
+  public void saveFile(String file, String str) {
+
+    // try-with-resources
+    try (FileOutputStream fileOutputstream = openFileOutput(file,
+            Context.MODE_PRIVATE);){
+
+      fileOutputstream.write(str.getBytes());
+      System.out.println("出力が完了しました。");
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 
   @Override
   protected void onStop() {
-    // TODO Auto-generated method stub
     super.onStop();
     // Listenerの登録解除
     manager.unregisterListener(this);
@@ -38,7 +86,6 @@ public class MainActivity extends Activity implements SensorEventListener {
 
   @Override
   protected void onResume() {
-    // TODO Auto-generated method stub
     super.onResume();
     // Listenerの登録
     List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ACCELEROMETER);
@@ -50,12 +97,11 @@ public class MainActivity extends Activity implements SensorEventListener {
 
   @Override
   public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    // TODO Auto-generated method stub
+
   }
 
   @Override
   public void onSensorChanged(SensorEvent event) {
-    // TODO Auto-generated method stub
     if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
       float accX = event.values[SensorManager.DATA_X];
       float accY = event.values[SensorManager.DATA_Y];
@@ -63,10 +109,10 @@ public class MainActivity extends Activity implements SensorEventListener {
       float combine = (float)sqrt(accX*accX + accY*accY + accZ*accZ);
 
       String str = "加速度センサの値"
-          + "\nX: " + accX
-          + "\nY: " + accY
-          + "\nZ: " + accZ
-          + "\n合成加速度: " + combine;
+              + "\nX: " + accX
+              + "\nY: " + accY
+              + "\nZ: " + accZ
+              + "\n合成加速度: " + combine;
       values.setText(str);
     }
   }
