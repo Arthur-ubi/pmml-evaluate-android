000138efe46638ebccbe188b1e4131d0f5a8d871
arthur-ubi <arthur@ubi.cs.ritsumei.ac.jp>
2018-09-16 12:31:05 +0900

    add sampling button

--- app/src/main/java/com/example/arthur/anti_asp/MainActivity.java
+++ app/src/main/java/com/example/arthur/anti_asp/MainActivity.java
@@ -13,7 +13,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.arthur.anti_asp.R;
 
 import static java.lang.Math.sqrt;
